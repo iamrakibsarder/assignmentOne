a2831370f4c4eb6ca68d39fdf0fc3ddfc178f857
Iamrakibsarder <iamrakibsarderofficial@gmail.com>
2022-12-05 13:01:22 +0600

    Updated Phone description with Variant for Bonus

--- mobile.py
+++ mobile.py
@@ -1,3 +1,5 @@
+import random
+
 mobile_data = {
     'status': True,
     'data': [
@@ -19,10 +21,28 @@ exchange_rate = mobile_data.get('exchnage_rate')
 splitted_price = mobile_data.get('data')[0].get('price').split()
 bdt = int(splitted_price[0]) * exchange_rate
 
+first_sentence = [
+    f'{phone_name} is one of the Best phone in the market',
+    f'{phone_name} is the latest model of Xiaomi',
+    f'The phone that comes with great display and camera is {phone_name}'
+]
+second_sentence = [
+    f'It is made in {country}',
+    f'{country} made this Awesome phone',
+    f'{country} is the place where it made'
+]
+third_sentence = [
+    f"It's price is about {price} which is {bdt} in Bangladeshi Taka",
+    f'The phone costs {price} or {bdt} Taka',
+    f'One need to have {price} or {bdt} Bangladeshi Taka to purchase the phone'
+]
+randomChoice1 = random.choice(first_sentence)
+randomChoice2 = random.choice(second_sentence)
+randomChoice3 = random.choice(third_sentence)
+sentence = f'{randomChoice1}. {randomChoice2}. {randomChoice3}'
 
+# description = f"{phone_name} is one of the Best phone in the market. It is made in {country}. It's price is about {price} which is {bdt} in Bangladeshi Taka"
 
-description = f"{phone_name} is one of the Best phone in the market. It is made in {country}. It's price is about {price} which is {bdt} in Bangladeshi Taka"
-
-print(description)
+print(sentence)
 
 
